fe4d22a238e8ecde9ea4bf3634030c888c5499e1
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-18 18:10:21 +0100

    add: eventlistener cell

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,7 @@
 // HTML ELEMENTS
 const playButton = document.getElementById('play-button')
 const square = document.getElementById('square')
-let grid;
-
+const box = document.querySelectorAll('.box')
 
 // GENERARE LA GRIGLIA AL CLICK DI PLAYBUTTON
 
@@ -10,15 +9,32 @@ playButton.addEventListener('click', function () {
     // rimuovere d-none dalla griglia
     square.classList.remove('d-none')
     //generare con un ciclo for quanti box necessari
-    const box = `<div class="box text-center" id="box"><span class="align-middle"></span></div>`
-    grid = boxGeneration(box)
-    
+    const grid = creatGrid(box)
+
 })
 
-function boxGeneration(element) {
-    for (i = 1; i <= 100 ; i++) {
-        square.innerHTML += element
+// al click della cella, questa si colora di azzurro
+
+
+
+function creatGrid() {
+    for (i = 1; i <= 100; i++) {
+        const cell = creatCell(i)
+        square.append(cell)
         console.log(i)
     }
 }
 
+
+
+function creatCell(i) {
+    const item = document.createElement('div')
+    item.classList.add('box')
+    item.classList.add('text-center')
+    item.innerHTML = i
+    item.addEventListener('click', function(){
+    item.classList.toggle('clicked')
+    })
+    return item;
+    
+}
