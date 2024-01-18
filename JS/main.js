1f51775aecde83dc7893381223e417cef14037e2
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-18 16:58:56 +0100

    add: grid

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,24 @@
 // HTML ELEMENTS
 const playButton = document.getElementById('play-button')
 const square = document.getElementById('square')
+let grid;
+
 
 // GENERARE LA GRIGLIA AL CLICK DI PLAYBUTTON
 
-playButton.addEventListener('click', function(){
+playButton.addEventListener('click', function () {
+    // rimuovere d-none dalla griglia
     square.classList.remove('d-none')
+    //generare con un ciclo for quanti box necessari
+    const box = `<div class="box text-center" id="box"><span class="align-middle"></span></div>`
+    grid = boxGeneration(box)
+    
 })
+
+function boxGeneration(element) {
+    for (i = 1; i <= 100 ; i++) {
+        square.innerHTML += element
+        console.log(i)
+    }
+}
+
