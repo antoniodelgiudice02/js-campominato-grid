da769f55f07d9a710222298691e6e440675a4deb
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-18 18:32:55 +0100

    update

--- JS/main.js
+++ JS/main.js
@@ -2,12 +2,16 @@
 const playButton = document.getElementById('play-button')
 const square = document.getElementById('square')
 const box = document.querySelectorAll('.box')
+const jumbotron = document.getElementById('jumbotron')
+const game = document.getElementById('game')
 
 // GENERARE LA GRIGLIA AL CLICK DI PLAYBUTTON
 
 playButton.addEventListener('click', function () {
     // rimuovere d-none dalla griglia
     square.classList.remove('d-none')
+    jumbotron.classList.add('d-none')
+    game.classList.remove('d-none')
     //generare con un ciclo for quanti box necessari
     const grid = creatGrid(box)
 
@@ -31,9 +35,13 @@ function creatCell(i) {
     const item = document.createElement('div')
     item.classList.add('box')
     item.classList.add('text-center')
+    item.classList.add('notclicked')
     item.innerHTML = i
     item.addEventListener('click', function(){
     item.classList.toggle('clicked')
+    item.classList.toggle('notclicked')
+    console.log(i)
+
     })
     return item;
     
